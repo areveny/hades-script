ff8db3ebd91cd1dd65da2a9013c483170882a8dd
areveny <areveny@protonmail.com>
2021-12-07 22:06:43 -0800

    Provisional commit of ingestion DB and ingestion logic before JSON interpreter rewrite

--- ingest.py
+++ ingest.py
@@ -1,14 +1,20 @@
+import typing
+from ingestion_db import IngestionDB, SqliteIngestionDB
+
+
 class Ingestion():
 
-    def __init__(self, file):
+    def __init__(self, file, ingestion_db):
         f = open(file, 'r')
         self.names_stack = list()
-        self.data = dict()
+        self.data_objects = list()
+        self.ingestion_db = ingestion_db
 
         code = iter(f)
         self.populate(code)
 
-    def populate(self, code):
+    def populate(self, code: typing.IO):
+        data_objects = list()
         try:
             while code:
                 cur_line = next(code)
@@ -16,23 +22,45 @@ class Ingestion():
         except StopIteration:
             pass
 
-    def find_assignment_or_table(self, code, cur_line):
+    def find_assignment_or_table(self, code: typing.IO, cur_line: str):
         """Processes unassigned tables and assignments of tables to a name"""
         print(self.names_stack, cur_line[:-1])
-        if "HadesAboutGhostAdmin02" in self.names_stack:
-            raise StopIteration
+        # if "HadesPostFlashback01" in self.names_stack:
+        if "TemporaryImprovedWeaponTrait_Patroclus" in cur_line:
+            breakpoint()
+            # raise StopIteration
+            pass
         while code:
-            if '{' in cur_line and '--' not in cur_line: # Found unassigned table
+            comment_loc = cur_line.find('--')
+            bracket_loc = cur_line.find('{')
+            close_bracket_loc = cur_line.find('}')
+            assign_loc = cur_line.find('=')
+
+            if comment_loc != -1 and comment_loc < bracket_loc and comment_loc < assign_loc:
+                cur_line = next(code)
+                continue
+            if bracket_loc != -1 and \
+                    not(assign_loc != -1 and assign_loc < bracket_loc): # Found unassigned table 
                 cur_line = '{'.join(cur_line.split('{')[1:]) # Get everything after the open bracket
                 self.find_assignment_or_table(code, cur_line)
-                cur_line = '}'.join(cur_line.split('}')[1:]) # Get everything after the open bracket
+                cur_line = '}'.join(cur_line.split('}')[1:]) # Get everything after the close bracket
 
-            if "Cue" in cur_line:
+            if " Cue " in cur_line:
                 parsed_object = self.process_voice_line(code, cur_line)
                 print(parsed_object)
+
+                if self.ingestion_db.can_process_object(parsed_object):
+                    try:
+                        self.data_objects.append({'line_name': parsed_object['Cue'], 
+                            'conversation_name': self.names_stack[-1], 
+                            'speaker': get_speaker_from_name(parsed_object['Cue']),
+                            'text': parsed_object['Text']})
+                    except KeyError:
+                        pass
                 return
 
-            if '=' in cur_line and '--' not in cur_line: # Found assignment
+            if assign_loc != -1 and \
+                    not(close_bracket_loc != -1 and close_bracket_loc < assign_loc): # Found assignment
                 split_line = cur_line.split(' =')
                 candidate_name = split_line[0].strip()
                 cur_line = ' ='.join(split_line[1:])
@@ -40,11 +68,11 @@ class Ingestion():
                 # Try and find out if we have a table or a simple assignment
                 while code:
                     if '{' in cur_line: # Table
-                        cur_line = '{'.join(cur_line.split('{')[1:]) # Get everything after the open bracket
+                        cur_line = cur_line[cur_line.find('{') + 1:] # Get everything after the open bracket
                         self.names_stack.append(candidate_name)
                         self.find_assignment_or_table(code, cur_line)
                         self.names_stack.pop()
-                        cur_line = '}'.join(cur_line.split('}')[1:]) # Get everything after the open bracket
+                        cur_line = cur_line[cur_line.find('}') + 1:] # Get everything after the close bracket
                         break
                     elif ',' in cur_line: # Found ',' before '}': simple assignment
                         break
@@ -56,25 +84,27 @@ class Ingestion():
 
             cur_line = next(code)
 
+    def process_db(self):
+        return
+
     def process_voice_line(self, code, cur_line):
         """Converts the `Cue` object starting at cur_line from code to an object"""
         cur_object = dict()
         while True:
-            self.set_key_values(cur_line, cur_object) # Consume assignments from line
+            Ingestion.set_key_values(cur_line, cur_object) # Consume assignments from line
             
             if '}' in cur_line: # Done with this assignment
                 return cur_object
             
             cur_line = next(code)
 
-    def set_key_values(self, line, object):
+    def set_key_values(line, object: dict):
         """Extracts assignments in `line` into object"""
         while '=' in line: # More assignments exist in remaining line
-            key, value, line = self.consume_assignment_in_line(line)
+            key, value, line = Ingestion.consume_assignment_in_line(line)
             object[key] = value
 
-
-    def consume_assignment_in_line(self, line):
+    def consume_assignment_in_line(line: str):
         """Pulls out the first assignment in a line and returns the remaining line"""
         end_assignment = line.find('=') # Find the end of the assignment statement
         name = line[:end_assignment].strip()
@@ -96,5 +126,12 @@ class Ingestion():
 
         return (name, value, line)
 
+def get_speaker_from_name(line_name):
+    if 'Zagreus' in line_name:
+        return 'Zagreus'
+    else:
+        return line_name[4:line_name.find('_')]
+
 if __name__ == '__main__':
-    npc_ingest = Ingestion('raw-data/NPCData.lua')
+    sqllite_ingestion = SqliteIngestionDB('hades-index.db')
+    npc_ingest = Ingestion('raw-data/NPCData.lua', sqllite_ingestion)
---
+++ ingestion_db.py
@@ -0,0 +1,45 @@
+import sqlite3 
+
+class IngestionDB():
+
+    def can_process_object(self, object):
+        raise NotImplemented
+
+    def process_object(self, object):
+        raise NotImplemented
+
+class SqliteIngestionDB(IngestionDB):
+
+    def __init__(self, db_file):
+        self.db_file = db_file
+        self.initialize()
+
+    def initialize(self):
+        con = sqlite3.connect(self.db_file)
+        with con:
+            con.execute("""DROP TABLE lines;""")
+            con.execute("""CREATE TABLE lines (
+            line_name TEXT PRIMARY KEY,
+            conversation_name TEXT,
+            speaker TEXT,
+            text TEXT
+            );
+            """)
+            con.execute("""CREATE INDEX speaker ON lines (speaker, conversation_name);""")
+            con.execute("""CREATE INDEX conversations ON lines (conversation_name, speaker);""")
+        con.close()
+
+    def can_process_object(self, obj):
+        return 'Cue' in obj and 'Text' in obj
+
+    def process_lines_dataset(self, line_data):
+        db_values = [(line_data['line_name'], line_data['conversation_name'], line_data['speaker'], line_data['text'])]
+
+        con = sqlite3.connect(self.db_file)
+        with con:
+            con.executemany('INSERT INTO lines (line_name, conversation_name, speaker, text) VALUES (?, ?, ?, ?)', db_values)
+        con.close()
+
+if __name__ == '__main__':
+    con = sqlite3.connect('hades-index.db')
+    
